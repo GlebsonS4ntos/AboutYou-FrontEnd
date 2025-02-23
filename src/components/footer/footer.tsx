38c7488d35fe0f941385246df4a6f53c63cf7974
Glebson Santos <glebsonsantos@outlook.pt>
2025-02-23 11:00:34 -0300

    feat: aplication footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex h-10 w-full items-center justify-center bg-gray-800 text-white font-mono text-sm font-bold">
+      <span>
+        ©Create by <a href="https://github.com/GlebsonS4ntos" target="_blank" className="text-red-500">Glebson Santos</a>
+      </span>
+    </footer>
+  );
+}
