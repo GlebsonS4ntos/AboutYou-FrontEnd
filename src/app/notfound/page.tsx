480cd9e3f607b8772d626f7c012891f568048aa2
Glebson Santos <glebsonsantos@outlook.pt>
2025-02-23 21:10:20 -0300

    feat: not found page

---
+++ src/app/notfound/page.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="fixed inset-0 bg-gray-900 flex items-center justify-center">
+            <span>About You não encontrado, <a href="/" className="text-red-500">Crie já o seu {"❤️"}</a></span>
+        </div>
+    );
+}
