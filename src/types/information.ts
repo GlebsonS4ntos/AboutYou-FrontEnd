d7be14e50fa5cc89a7dbf0168670fb21946d0e7f
Glebson Santos <glebsonsantos@outlook.pt>
2025-03-02 09:36:56 -0300

    feat: create information type

---
+++ src/types/information.ts
@@ -0,0 +1,13 @@
+export interface Information {
+  id: string;
+  profession: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  linkedlnUrl: string;
+  githubUrl: string;
+  instagramUrl: string;
+  facebookUrl: string;
+  whatsapp: string;
+  email: string;
+}
