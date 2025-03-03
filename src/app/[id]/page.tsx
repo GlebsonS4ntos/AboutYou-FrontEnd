7be15a68eb10904acea7d27ed690e528c0b7e675
Glebson Santos <glebsonsantos@outlook.pt>
2025-03-02 22:44:07 -0300

    feat: about page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3,28 +3,25 @@
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react";
+import Header from "@/components/aboutpage/header";
+import Content from "@/components/aboutpage/content";
+import { Information } from '@/types';
 
-interface Information {
-  id: string;
-  profession: string;
-  name: string;
-  description: string;
-  imageUrl: string;
-  linkedlnUrl: string;
-  githubUrl: string;
-  instagramUrl: string;
-  facebookUrl: string;
-  whatsapp: string;
-  email: string;
-}
-
-export default function Page({ params }: { params: Promise<{ id: string }>}) {
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const route = useRouter();
   const [infoData, setInfoData] = useState<Information | null>(null);
   const [loading, setLoading] = useState(true);
   const paramsId = use(params);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
     const getInformation = async () => {
       try {
         const response = await fetch(
@@ -52,18 +49,49 @@ export default function Page({ params }: { params: Promise<{ id: string }>}) {
     getInformation();
   }, []);
 
-
   if (loading) {
     return (
-        <div className="fixed inset-0 bg-gray-900 flex items-center justify-center">
-            <Loader className="animate-spin text-white w-8 h-8"/> : 
-        </div>
+      <div className="fixed inset-0 bg-gray-900 flex items-center justify-center">
+        <Loader className="animate-spin text-white w-8 h-8" /> :
+      </div>
     );
   }
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline"></h1>
+      <div className="flex w-full min-h-screen bg-gray-800">
+        {isMobile ? (
+          <></>
+        ) : (
+          <div className="flex items-center justify-center w-1/2 min-h-screen">
+            <Header
+              infoData={infoData}
+              imgHeight={256}
+              imgWidth={256}
+              textSize="text-4xl"
+              isMobile={isMobile}
+            />
+          </div>
+        )}
+        <div
+          className={`flex justify-center flex-col ${
+            isMobile ? "w-full" : "w-1/2"
+          }`}
+        >
+          {!isMobile ? (
+            ""
+          ) : (
+            <Header
+              infoData={infoData}
+              imgHeight={128}
+              imgWidth={128}
+              textSize="text-2xl"
+              isMobile={isMobile}
+            />
+          )}
+          <Content infoData={infoData}/>
+        </div>
+      </div>
     </>
   );
 }
